c8fa4626c0c9ad86f472be2d369d9c614680b89f
AnushkaPraveen <anushkapraveen98@gmail.com>
2024-01-28 13:44:08 +0530

    experience section and projects section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 
 import styles from './App.module.css'
+import { About } from './components/About/About'
+import { Experience } from './components/Experience/Experience'
 import { Hero } from './components/Hero/Hero'
-import { Navbar } from './components/Navbar'
+import { Navbar } from './components/Navbar/Navbar'
+import { Projects } from './components/Projects/Projects'
 
 function App() {
   
@@ -10,6 +13,9 @@ function App() {
     <div className={styles.App}>
      <Navbar/>
      <Hero/>
+     <About/>
+     <Experience/>
+     <Projects/>
     </div>
   )
 }
